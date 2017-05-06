681799c6fcebcc391ee78a266469a587855399c2
Juan E Sanchez <juanesanchez@github>
2017-05-06 13:18:59 -0500

    update testing

--- run_tests.sh
+++ run_tests.sh
@@ -4,11 +4,13 @@ BASEDIR="${PWD}"
 TAG=${1}
 TAGDIR=devsim_win64_${TAG}
 TAGTGZ=${TAGDIR}.tgz
-DEVSIM_PY=${TAGDIR}/bin/devsim
-DEVSIM_TCL=${TAGDIR}/bin/devsim_tcl
+DEVSIM_PY=${BASEDIR}/${TAGDIR}/bin/devsim
+DEVSIM_TCL=${BASEDIR}/${TAGDIR}/bin/devsim_tcl
 ANACONDA_PATH=/cygdrive/C/Miniconda-x64
 CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake
 CTEST=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/ctest
+DEVSIM_PY_EXE=${BASEDIR}/${TAGDIR}/bin/devsim
+DEVSIM_TCL_EXE=${BASEDIR}/${TAGDIR}/bin/devsim_tcl
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
@@ -17,11 +19,11 @@ mkdir -p bin
 
 # symlinks do not work on windows
 for i in testing examples; do \
-  rm -rf $i; \
+  #rm -rf $i; \
   cp -r ${TAGDIR}/$i .; \
 done
   
 
 rm -rf run && mkdir run
-(cd run && "${CMAKE}" -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY_TEST_EXE=${TAGDIR}/bin/devsim -DDEVSIM_TCL_TEST_EXE=${TAGDIR}/bin/devsim_tcl ..)
+(cd run && "${CMAKE}" -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY_TEST_EXE=${DEVSIM_PY_EXE} -DDEVSIM_TCL_TEST_EXE=${DEVSIM_TCL_EXE} ..)
 (cd run && "${CTEST}" -j2)

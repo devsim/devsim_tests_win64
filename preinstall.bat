ea52ad9ece7d558301cdec08ce6d15392f769722
Juan E. Sanchez <juan@tcad.com>
2019-04-24 18:56:42 -0500

    update preinstall script

--- preinstall.bat
+++ preinstall.bat
@@ -1,6 +1,6 @@
 
-conda create -y --name=python27 python=2.7
-conda install -y --name=python27 numpy mkl tk cmake
-conda create -y --name=python37 python=3.7
-conda install -y --name=python37 numpy mkl tk
+call conda create -y --name=python27 python=2.7
+call conda install -y --name=python27 numpy mkl tk cmake zlib sqlite
+call conda create -y --name=python37 python=3.7
+call conda install -y --name=python37 numpy mkl tk zlib sqlite
 

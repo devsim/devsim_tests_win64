49f1f317ea0cbfeda338f2c8519f884f404ae3ec
Juan E. Sanchez <juan@tcad.com>
2021-11-30 22:18:59 -0600

    update tests

--- preinstall.bat
+++ preinstall.bat
@@ -1,4 +1,4 @@
 
-call conda create -y --name=python37 python=3.7
-call conda install -y --name=python37 numpy mkl zlib sqlite cmake
+call conda create -y --name=devsim_test python=3 numpy mkl zlib sqlite cmake
+call conda activate devsim_test
 
